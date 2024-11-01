482813eaed41de796618725556fd2f72f635db14
Phaedra144 <bedi.szilvia@gmail.com>
2024-11-01 13:02:41 +0100

    Create a controller

---
+++ todoapp/src/main/java/com/spring/react/todoapp/controller/TodoController.java
@@ -0,0 +1,26 @@
+package com.spring.react.todoapp.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.react.todoapp.dtos.TodoDto;
+import com.spring.react.todoapp.service.TodoService;
+
+@RestController
+public class TodoController {
+
+  TodoService todoService;
+
+  public TodoController(TodoService todoService) {
+    this.todoService = todoService;
+  }
+
+  @GetMapping("users/{userName}/todos")
+    public List<TodoDto> getAllTodos(@PathVariable String userName) {
+        return todoService.findAllByUsername(userName);
+    }
+
+}
