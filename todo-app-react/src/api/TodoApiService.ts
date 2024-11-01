d680a56e78584c45dbeb2a18b0a37d69baa01190
Phaedra144 <bedi.szilvia@gmail.com>
2024-11-01 14:00:36 +0100

    Add axios and create api service

---
+++ todo-app-react/src/api/TodoApiService.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:8080/api/v1',
+});
+
+export const getTodos = (userName: string) => {
+  return apiClient.get(`/users/${userName}/todos`);
+};
+
+export const getTodoById = (userName: string, todoId: number) => {
+  return apiClient.get(`/users/${userName}/todos/${todoId}`);
+};
+
+export const deleteTodo = (userName: string, todoId: number) => {
+  return apiClient.delete(`/users/${userName}/todos/${todoId}`);
+};
